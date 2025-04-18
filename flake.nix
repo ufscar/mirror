11aefedaa7d73fd0b62a84e6eec04fa35485476e
Gabriel Fontes <hi@m7.rs>
2025-04-18 16:43:35 -0300

    chore: format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,31 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-    archvsync.url = "github:LuNeder/archvsync-nix";
-    archvsync.inputs.nixpkgs.follows = "nixpkgs";
-    deploy-rs.url = "github:serokell/deploy-rs";
-    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    archvsync = {
+      url = "github:LuNeder/archvsync-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = inputs@{ self, nixpkgs, deploy-rs, ... }: {
-    nixosConfigurations.mirror = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        inputs.disko.nixosModules.disko
-      ];
-      specialArgs = {inherit inputs;};
+    nixosConfigurations = {
+      mirror = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          inputs.disko.nixosModules.disko
+        ];
+        specialArgs = {inherit inputs;};
+      };
     };
     deploy.nodes = let
       activate = kind: config: deploy-rs.lib.${config.pkgs.system}.activate.${kind} config;
